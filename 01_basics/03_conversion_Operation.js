b768f27bfe3dc06691e47fc25a51d21b996ae5b3
ganeshvalere <147713881+ganeshvalere@users.noreply.github.com>
2024-04-03 09:31:24 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion_Operation.js
@@ -0,0 +1,26 @@
+// let score = "33ads"
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueNumber = Number (score)
+// console.log(valueNumber);
+
+
+// "33" => 33
+// "22abc" => Nan
+// true => 1; false => 0
+
+
+let booleanIsLogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLogedIn);
+
+// 1 => truw; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
