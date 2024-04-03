4668592e950696791ca62323211a58a1ac7b94bf
ganeshvalere <147713881+ganeshvalere@users.noreply.github.com>
2024-04-03 07:41:04 +0000

    variable done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountID = 1213
+let accountEmail = "ganesh@gnmail.com"
+var acccountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountID = 2 // not allowed
+
+accountEmail = "jnvejr@gmail.com"
+acccountPassword = "1436"
+accountCity = "Benglru"
+
+
+console.log(accountID);
+console.table([accountID, accountEmail, acccountPassword, accountCity, accountState])
+
+
+
